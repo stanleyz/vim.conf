756b6fcb901d78c595ad9cae2aa8a0a4f448bb74
Stanley Zhang <stanley.zhang@ityin.net>
2016-10-01 22:52:59 +1300

    Linux keys

---
+++ autokey/ESC_CAPS/convert_esc_caps.py
@@ -0,0 +1,6 @@
+# Enter script code
+# Autokey doesn't work well with Chrome, terminator:
+# 1. You can't use UP/DOWN on search input of google.com in Chrome
+# Same thing to search input field of yahoo.com
+# But it works fine in Firefox.
+# 2. terminator will blink when use UP/DOWN
